f39718ea5a2f1f25e1b5eb370cb65dd74888aecb
Nikolay <nikolay_d2000@mail.ru>
2022-12-06 20:23:01 +0300

    Finish

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'dronikolay2000@gmail.com';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика (Пороль приложений)
+$mail->Password = 'cmjnqlbfmwrqholr';                           // Наш пароль от ящика (Пороль приложений)
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  

1522c5ea5a2cf413eccadbed12b5bfa477a09c6f
Nikolay <nikolay_d2000@mail.ru>
2024-05-15 19:55:59 +0300

    Finish - fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function () {
 
@@ -17,17 +19,56 @@ gulp.task('server', function () {
 });
 
 gulp.task('styles', function () {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass|css)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function() {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"))
+});
+
+
+gulp.task('scripts', function() {
+    return gulp.src("src/js**/*.js")
+        .pipe(gulp.dest("dist/js"))
+});
+
+
+gulp.task('fonts', function() {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+});
+
+
+gulp.task('ico', function() {
+    return gulp.src("src/ico/**/*")
+        .pipe(gulp.dest("dist/ico"))
+});
+
+
+gulp.task('mailer', function() {
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest("dist/mailer"))
+});
+
+
+gulp.task('images', function() {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"))
+});
+
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'ico', 'mailer', 'images'));
